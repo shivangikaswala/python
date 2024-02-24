b33a55b46f907647cff373306f1dc49217bac3e9
shivangikaswala <161157095+shivangikaswala@users.noreply.github.com>
2024-02-24 19:29:06 +0100

    first commit

---
+++ First_program.py
@@ -0,0 +1,12 @@
+a = input('What is your birth city name?\n')
+b = input('what is your current city?\n')
+c = input('What is your favourite pet?\n')
+print (f"Hello! Your birth city name is {a}"+f"Your current city name is {b}"+f"Your favourite pet is {c}.")
+
+
+
+
+
+
+
+
